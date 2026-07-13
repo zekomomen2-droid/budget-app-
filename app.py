e74f741e856707dfc19084484c5defaef693f9b5
zekomomen2-droid <zekomomen2@gmail.com>
2026-07-13 15:03:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.title("💰 نظام إدارة الميزانية الشخصية")
 
-income = st.number_input("أدخل دخلك الشهري:", min_value=0.0)
-expense = st.number_input("أدخل مصروفاتك:", min_value=0.0)
+income = st.number_input("أدخل دخلك الشهري:", min_value=0)
+expense = st.number_input("أدخل مصروفاتك:", min_value=0)
 
 if st.button("احسب الرصيد"):
     balance = income - expense
